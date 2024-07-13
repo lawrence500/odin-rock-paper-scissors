5f8cb9894b63e575665f6c7a298a57fc7ccbd042
lawrence <knightboy500@gmail.com>
2024-07-13 14:01:58 +0100

    build: finsihed final touches

--- script.js
+++ script.js
@@ -1,47 +1,75 @@
-const choices = ['rock', 'paper', 'scissors']
-let humanScore = 0
-let computerScore = 0
+const choices = ["rock", "paper", "scissors"];
+let humanScore = 0;
+let computerScore = 0;
+let round = 0
 
 const getComputerChoice = () => {
-    let computerChoice = Math.floor(Math.random() * choices.length)
-    return choices[computerChoice]
-}
+  let computerChoice = Math.floor(Math.random() * choices.length);
+  return choices[computerChoice];
+};
 
-const getHumanChoice = prompt('choose (rock, paper, scissors)')
+const getHumanChoice = () => {
+  let humanChoice = prompt("choose (rock, paper, scissors)");
+  return humanChoice;
+};
 
 const rewardText = (winner) => {
-    return console.log(`${winner} wins 1 point`)
+  return console.log(`${winner} wins 1 point`);
+};
+
+function playRound(humanChoice, computerChoice) {
+  humanChoice = humanChoice.toLowerCase();
+    round++
+
+    if (round >= 6){
+      if(humanScore > computerScore){
+        alert(`game over! human won\n human-points: (${humanScore})\n computer-points: (${computerScore})`)
+        clearInterval(startGame)
+      } else{
+        alert(`game over! computer won\n computer-points(${computerScore})\n human-points: (${humanScore})`)
+        clearInterval(startGame)
+      }
+    }
+
+    if (humanChoice === computerChoice) {
+      console.log("draw...");
+    }
+  
+    if (humanChoice === "paper" && computerChoice === "rock") {
+      rewardText("human");
+      humanScore++;
+    } else if (computerChoice === "paper" && humanChoice === "rock") {
+      rewardText("computer");
+      computerScore++;
+    }
+  
+    if (humanChoice === "scissors" && computerChoice === "paper") {
+      rewardText("human");
+      humanScore++;
+    } else if (computerChoice === "scissors" && humanChoice === "paper") {
+      rewardText("computer");
+      computerScore++;
+    }
+  
+    if (humanChoice === "rock" && computerChoice === "scissors") {
+      rewardText("human");
+      humanScore++;
+    } else if (computerChoice === "rock" && humanChoice === "scissors") {
+      rewardText("computer");
+      computerScore++;
+    }  
 }
 
-function playRound(humanChoice, computerChoice){
-    humanChoice = humanChoice.toLowerCase()
-    console.log(humanChoice)
+function playGame() {
+  const computerSelection = getComputerChoice();
+  const humanSelection = getHumanChoice();
 
-    if(humanChoice === computerChoice){
-        console.log('draw...')
-    }
+  playRound(humanSelection, computerSelection)
 
-    if(humanChoice === 'paper' && computerChoice === 'rock'){
-        rewardText('human')
-    } else{
-        rewardText('computer')
-    }
+  return console.log(round)
 
-    if(humanChoice === 'scissors' && computerChoice === 'paper'){
-        rewardText('human')
-    } else{
-        rewardText('computer')
-    }
-
-    if(humanChoice === 'rock' && computerChoice === 'scissors'){
-        rewardText('human')
-    } else{
-        rewardText('computer')
-    }
 }
 
-const computerSelection = getComputerChoice()
-const humanSelection = getHumanChoice
-
-playRound(getHumanChoice, getComputerChoice)
-
+if(confirm('startgame')){
+let startGame  = setInterval(() => {playGame()}, 1000)
+}

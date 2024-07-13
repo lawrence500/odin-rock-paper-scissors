f814e216849a6a1e19b74a4c3fb7cc2b3f75b5c7
lawrence <knightboy500@gmail.com>
2024-07-13 13:06:33 +0100

    update : adds playgame functions

--- script.js
+++ script.js
@@ -9,3 +9,39 @@ const getComputerChoice = () => {
 
 const getHumanChoice = prompt('choose (rock, paper, scissors)')
 
+const rewardText = (winner) => {
+    return console.log(`${winner} wins 1 point`)
+}
+
+function playRound(humanChoice, computerChoice){
+    humanChoice = humanChoice.toLowerCase()
+    console.log(humanChoice)
+
+    if(humanChoice === computerChoice){
+        console.log('draw...')
+    }
+
+    if(humanChoice === 'paper' && computerChoice === 'rock'){
+        rewardText('human')
+    } else{
+        rewardText('computer')
+    }
+
+    if(humanChoice === 'scissors' && computerChoice === 'paper'){
+        rewardText('human')
+    } else{
+        rewardText('computer')
+    }
+
+    if(humanChoice === 'rock' && computerChoice === 'scissors'){
+        rewardText('human')
+    } else{
+        rewardText('computer')
+    }
+}
+
+const computerSelection = getComputerChoice()
+const humanSelection = getHumanChoice
+
+playRound(getHumanChoice, getComputerChoice)
+
